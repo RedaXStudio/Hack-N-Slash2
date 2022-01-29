71f6233c157b75a11145951f09f42d18813f0c8a
RedaXStudio <98216947+RedaXStudio@users.noreply.github.com>
2022-01-29 21:23:57 +0100

    Update LinesPoints.cs

--- Assets/Player/SkillTree/LinesPoints.cs
+++ Assets/Player/SkillTree/LinesPoints.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class LinesPoints : LineUI
 {
    public PassivePoint passivePoint1;
-   
    public PassivePoint passivePoint2;
+  
 }
+
